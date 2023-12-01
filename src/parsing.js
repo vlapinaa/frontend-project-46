ddce2c50d167b3a2f70d1ed8d1b91c43b670a20f
Varvara <lapinavr@gmail.com>
2023-12-01 15:52:07 +0700

    correcting eslint

--- src/parsing.js
+++ src/parsing.js
@@ -2,8 +2,7 @@ import path from 'node:path';
 import { readFileSync } from 'node:fs';
 import yaml from 'js-yaml';
 
-export const createAbsolutePath = (filepath) =>
-  path.resolve(process.cwd(), filepath);
+export const createAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
 export const parseFile = (filepath) => {
   const file = readFileSync(createAbsolutePath(filepath), { encoding: 'utf8' });
